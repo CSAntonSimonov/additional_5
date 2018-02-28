9a3926a5640eee8da3027a79f36f71f09b726db7
KoTuKJuBoTuK <simonovantond@gmail.com>
2018-02-28 16:38:17 +0300

    Make changes with regexs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-	for (var d = /(\u005B|\u0028|\u007B)[^\u005B\u0028\u007B]*?$/,
+    for (var d = /(\u005B|\u0028|\u007B)[^\u005B\u0028\u007B]*?$/,
      e = {
         "(": /\u0028[^\u007D\u005D]*?\u0029/,
         "[": /\u005B[^\u0029\u007D]*?\u005D/,
         "{": /\u007B[^\u0029\u005D]*?\u007D/,
-        //"|": /\u007C[^\u0029\u005D\u007D]*?\u007C/
     }, b, c = !0; c;) b = str, str = str.replace(d, function (str, b) {
         return str.replace(e[b], "")
     }), b == str && (c = !1);
